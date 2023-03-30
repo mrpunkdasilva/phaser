145e4c96516342107769b4ef4ef20ccdd1d1e349
Etec <etec@etec.com>
2023-03-29 22:15:12 -0300

    changes

--- app/src/main/java/etec/com/br/gustavohj/phaser/Adapter/ToDoAdapter.java
+++ app/src/main/java/etec/com/br/gustavohj/phaser/Adapter/ToDoAdapter.java
@@ -1,4 +1,37 @@
 package etec.com.br.gustavohj.phaser.Adapter;
 
-public class ToDoAdapter {
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+import etec.com.br.gustavohj.phaser.MainActivity;
+import etec.com.br.gustavohj.phaser.Model.ToDoModel;
+import etec.com.br.gustavohj.phaser.R;
+
+public class ToDoAdapter extends RecyclerView.Adapter<ToDoAdapter.ViewHolder> {
+
+    private List<ToDoModel> todoList;
+    private MainActivity activity;
+
+
+    public ToDoAdapter(MainActivity Activity) {
+        this.activity = activity;
+    }
+
+    public ToDoAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.quest_layout, parent, false);
+
+        return new RecyclerView.ViewHolder(itemView);
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+
+    }
+
 }
